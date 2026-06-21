03ecae8bf172e56318b8c2eb99a58f33033a0720
take44444 <take44444.general@gmail.com>
2026-06-21 18:33:56 +0900

    Add menu icon

---
+++ components/icons/lucide-menu.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+
+export function MenuIcon({
+  size = 24,
+  color = "currentColor",
+  strokeWidth = 2,
+  className,
+  ...props
+}: React.SVGProps<SVGSVGElement> & {
+  size?: number;
+  color?: string;
+  strokeWidth?: number;
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+      {...props}
+    >
+      <path d="M4 5h16M4 12h16M4 19h16"/>
+    </svg>
+  );
+}
