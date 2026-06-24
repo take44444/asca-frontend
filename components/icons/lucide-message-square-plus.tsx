e997a5d1c7983156efc98af1b7e1e5aa14ed7f5c
take44444 <take44444.general@gmail.com>
2026-06-25 01:38:20 +0900

    Add required components for implementing chat feature

---
+++ components/icons/lucide-message-square-plus.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+
+export function MessageSquarePlusIcon({
+  size = 24,
+  color = "currentColor",
+  strokeWidth = 2,
+  className,
+  ...props
+}: React.SVGProps<SVGSVGElement> & {
+  size?: number;
+  color?: string;
+  strokeWidth?: number;
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth={strokeWidth}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+      {...props}
+    >
+      <path d="M22 17a2 2 0 0 1-2 2H6.828a2 2 0 0 0-1.414.586l-2.202 2.202A.71.71 0 0 1 2 21.286V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2zM12 8v6m-3-3h6"/>
+    </svg>
+  );
+}
